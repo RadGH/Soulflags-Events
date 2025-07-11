3144fe8f44f0d5812aaa860c0f7043ea226c3a29
Radley <radleygh@gmail.com>
2025-07-11 09:39:58 -0700

    Fixed alignment of available classes shortcode

--- templates/parts/term-summary.php
+++ templates/parts/term-summary.php
@@ -7,15 +7,6 @@
 $term_url = get_term_link( $term );
 $is_singular_term = is_tax( 'class_type' );
 
-$featured_image_id = get_field( 'default_featured_image_id', 'class_type_' . $term->term_id );
-if ( ! $featured_image_id ) $featured_image_id = get_field( 'default_featured_image_id', 'sfe_settings' );
-
-if ( $featured_image_id ) {
-	echo '<div class="sfe-term-featured-image">';
-	echo wp_get_attachment_image( $featured_image_id, 'large', false, array( 'class' => 'sfe-term-featured-image__img' ) );
-	echo '</div>';
-}
-
 echo '<div class="sfe-term-summary">';
 
 // Display term title
